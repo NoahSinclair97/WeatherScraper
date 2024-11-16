bc0b81747928bd919b92ed2ef64f6c1d3cf014d3
Noah Sinclair <nsinclair3@academic.rrc.ca>
2024-11-15 20:00:55 -0600

    Added comment for file and imported html parser

--- scrape_weather.py
+++ scrape_weather.py
@@ -1,2 +1,14 @@
-class WeatherScraper:
+"""
+ ADEV-3005 (254275) Programming in Python
+ Name: Noah Sinclair
+ Student Number: 0385289
+ Final Project
+
+ A file for scraping weather data
+"""
+
+from html.parser import HTMLParser
+import urllib.request
+
+class WeatherScraper(HTMLParser):
   pass
