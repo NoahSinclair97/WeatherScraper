906d11565f1885d0186c20acc30bffbe1e9d1fc4
Noah Sinclair <nsinclair3@academic.rrc.ca>
2024-11-18 02:43:03 -0600

    Worked on weather scraper. Added db files skeleton

---
+++ db_operations.py
@@ -0,0 +1,12 @@
+class DBOperations:
+  def fetch_data():
+    pass
+
+  def save_data():
+    pass
+
+  def initialize_db():
+    pass
+
+  def purge_data():
+    pass
--- scrape_weather.py
+++ scrape_weather.py
@@ -9,6 +9,48 @@
 
 from html.parser import HTMLParser
 import urllib.request
+import datetime
 
 class WeatherScraper(HTMLParser):
-  pass
+  """
+  A class for parsing html
+  """
+  warning = "search by station name"
+  def handle_starttag(self, tag, attrs):
+    """
+    Handles start tag of an element
+    """
+    #print("Found start tag :", tag)
+
+
+  def handle_endtag(self, tag):
+    """
+    Handles end tag of an element
+    """
+    #print("Found end tag :", tag)
+
+
+  def handle_data(self, data):
+    """
+    Handles data of an element
+    """
+    #print("Found some data :", data)
+    if self.warning in data.lower():
+      print("The page needs correct data")
+
+
+def get_ip():
+  """
+  Gets the current ip address.
+  """
+  year = datetime.datetime.year
+  month = datetime.datetime.month
+
+  myparser = WeatherScraper()
+  with urllib.request.urlopen(f'http://climate.weather.gc.ca/climate_data/daily_data_e.html?StationID=27174&timeframe=2&StartYear=1740&EndYear=2018&Day=1&Year=2024&Month=12#') as response:
+    html = str(response.read())
+  myparser.feed(html)
+  return myparser
+
+if __name__ == "__main__":
+  print(get_ip())
